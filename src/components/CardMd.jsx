8b5fbc4e57efb597215d66d46945ae567990773a
SP-Upreti <surendraupreti441@gmail.com>
2024-09-07 14:26:13 +0545

    data update

--- src/components/CardMd.jsx
+++ src/components/CardMd.jsx
@@ -5,11 +5,11 @@ export default function CardMd({ id }) {
     return (
         <div className="border w-[275px] overflow-hidden group cursor-pointer rounded-lg">
             <div className="h-[160px] overflow-hidden">
-                <img src="/keyboard.png" alt="" className="transition-all group-hover:scale-[1.4]" />
+                <img src="/tractor.png" alt="" className="transition-all group-hover:scale-[1.4]" />
             </div>
             <div className="p-2">
                 <div className="mt-3">
-                    <h2 className="text-xl font-bold">High Quality Mechanical Keyboard</h2>
+                    <h2 className="text-xl font-bold">Heavy electric tractor mahendra</h2>
                     <p className="font-bold tracking-wider mt-3">Rs. 5500/-</p>
                 </div>
                 <div className=" flex gap-4 items-center mt-4">
--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,14 +1,16 @@
 export default function Sales() {
     return (
-        <section className="h-[417px] bg-white border overflow-hidden my-16 relative flex justify-end items-center px-[9rem]">
-
+        <section className="h-[417px] text-white border overflow-hidden bg-[#123a08] my-16 relative flex justify-evenly  items-center  px-[9rem]">
+            <div className=" h-[90%]">
+                <img src="/sale.png" alt="" className="h-full" />
+            </div>
             <div className="z-[99] ">
                 <h2 className="text-3xl font-bold">Sales upto 50% off</h2>
                 <h3 className="text-xl text-center">12 inch HD Display</h3>
 
                 <div className=" flex gap-4 justify-center items-center mt-8 font-bold">
-                    <button className="bg-[--primary] border border-[--primary] text-white px-5 text-lg py-[4px] rounded-lg">Buy</button>
-                    <button className="border border-[--primary] capitalize text-[--primary] px-5 text-lg py-[4px] rounded-lg">more</button>
+                    <button className="bg-[white] border border-[white] text-[--primary] px-5 text-lg py-[4px] rounded-lg">Buy</button>
+                    <button className="border border-[white] capitalize text-[white] px-5 text-lg py-[4px] rounded-lg">more</button>
                 </div>
             </div>
 
