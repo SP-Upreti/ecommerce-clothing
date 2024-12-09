6c90dd228deeabe31a7a2aa9d972594d6a9b810a
SP-Upreti <surendraupreti441@gmail.com>
2024-12-09 15:17:45 +0545

    Update page.js

--- src/app/customersupport/page.js
+++ src/app/customersupport/page.js
@@ -19,9 +19,7 @@ export default function Page() {
                             title="map"
                             marginHeight="0"
                             marginWidth="0"
-                            src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-                            style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}
-                        ></iframe>
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113032.71841725819!2d85.17023615627983!3d27.708873041514817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198a307baabf%3A0xb5137c1bf18db1ea!2sKathmandu%2044600%2C%20Nepal!5e0!3m2!1sen!2sus!4v1733736552118!5m2!1sen!2sus" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
                             <div className="lg:w-1/2 px-6">
                                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
@@ -30,7 +28,7 @@ export default function Page() {
                             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
                                 <a href="mailto:example@email.com" className="text-red-500 leading-relaxed">
-                                    example@email.com
+                                    myecommerce@email.com
                                 </a>
                                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
                                 <p className="leading-relaxed">123-456-7890</p>
@@ -38,7 +36,7 @@ export default function Page() {
                         </div>
                     </div>
                     <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                        <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+                        <h2 className="text-gray-900 text-lg mb-1 font-semibold title-font">Feedback</h2>
                         <p className="leading-relaxed mb-5 text-gray-600">
                             Post-ironic portland shabby chic echo park, banjo fashion axe
                         </p>
@@ -50,6 +48,7 @@ export default function Page() {
                                 type="text"
                                 id="name"
                                 name="name"
+                                placeholder='ram bahadur'
                                 className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
                         </div>
@@ -60,6 +59,7 @@ export default function Page() {
                             <input
                                 type="email"
                                 id="email"
+                                placeholder='ram@gmail.com'
                                 name="email"
                                 className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
@@ -71,6 +71,7 @@ export default function Page() {
                             <textarea
                                 id="message"
                                 name="message"
+                                placeholder='lorem ispum dollar sit'
                                 className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                             ></textarea>
                         </div>
