8ff9728a0b6b7224fa8d8baa961ea8126fa80c5e
SP-Upreti <surendraupreti441@gmail.com>
2024-12-11 13:40:06 +0545

    Update cart.jsx

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -82,47 +82,51 @@ export default function Cart() {
                                         Continue Shopping
                                     </Link>
                                 </div>
-                                <div id="summary" className="w-full sm:w-1/4 md:w-1/2 px-8 py-10">
-                                    <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
-                                    <div className="flex justify-between mt-10 mb-5">
-                                        <span className="font-semibold text-sm uppercase">Items {items.length}</span>
-                                        <span className="font-semibold text-sm">590$</span>
-                                    </div>
-                                    <div>
-                                        <label className="font-medium inline-block mb-3 text-sm uppercase">
-                                            Shipping
-                                        </label>
-                                        <select className="block p-2 text-gray-600 w-full text-sm">
-                                            <option>Standard shipping - $10.00</option>
-                                        </select>
-                                    </div>
-                                    <div className="py-10">
-                                        <label
-                                            htmlFor="promo"
-                                            className="font-semibold inline-block mb-3 text-sm uppercase"
-                                        >
-                                            Promo Code
-                                        </label>
-                                        <input
-                                            type="text"
-                                            id="promo"
-                                            placeholder="Enter your code"
-                                            className="p-2 text-sm w-full border"
-                                        />
-                                    </div>
-                                    <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">
-                                        Apply
-                                    </button>
-                                    <div className="border-t mt-8">
-                                        <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-                                            <span>Total cost</span>
-                                            <span>$600</span>
+                                {
+                                    items.length > 0 && (
+                                        <div id="summary" className="w-full sm:w-1/4 md:w-1/2 px-8 py-10">
+                                            <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+                                            <div className="flex justify-between mt-10 mb-5">
+                                                <span className="font-semibold text-sm uppercase">Items {items.length}</span>
+                                                <span className="font-semibold text-sm">590$</span>
+                                            </div>
+                                            <div>
+                                                <label className="font-medium inline-block mb-3 text-sm uppercase">
+                                                    Shipping
+                                                </label>
+                                                <select className="block p-2 text-gray-600 w-full text-sm">
+                                                    <option>Standard shipping - $10.00</option>
+                                                </select>
+                                            </div>
+                                            <div className="py-10">
+                                                <label
+                                                    htmlFor="promo"
+                                                    className="font-semibold inline-block mb-3 text-sm uppercase"
+                                                >
+                                                    Promo Code
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    id="promo"
+                                                    placeholder="Enter your code"
+                                                    className="p-2 text-sm w-full border"
+                                                />
+                                            </div>
+                                            <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">
+                                                Apply
+                                            </button>
+                                            <div className="border-t mt-8">
+                                                <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+                                                    <span>Total cost</span>
+                                                    <span>$600</span>
+                                                </div>
+                                                <button onClick={() => { setCheckout(true) }} className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+                                                    Checkout
+                                                </button>
+                                            </div>
                                         </div>
-                                        <button onClick={() => { setCheckout(true) }} className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
-                                            Checkout
-                                        </button>
-                                    </div>
-                                </div>
+                                    )
+                                }
                             </div>
                         </div>
                     )
